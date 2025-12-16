8396dcb1367524c8d1d7d67cf5493d3b2bfd66ac
Sudhir Singh <thesudhiir@gmail.com>
2025-12-16 14:37:19 +0530

    Update app.py
    changed os

--- app.py
+++ app.py
@@ -313,8 +313,8 @@ def analytics():
 # RUN
 # =====================================================
 if __name__ == "__main__":
-    import webbrowser
-    webbrowser.open("http://127.0.0.1:5000")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
